27753d370cb814bf64e201ee15895da6a76350ac
esaki01 <social0430@gmail.com>
2020-01-15 21:25:33 +0900

    Add queue test

---
+++ tests/data_structure/test_queue.py
@@ -0,0 +1,15 @@
+import unittest
+
+from src.data_structure.queue import Queue
+
+
+class TestQueue(unittest.TestCase):
+
+    def test_queue(self):
+        alist = Queue()
+        alist.enqueue(1)
+        alist.enqueue(2)
+        alist.enqueue(3)
+
+        self.assertEqual(1, alist.dequeue())
+        self.assertEqual([2, 3], alist.queue)
