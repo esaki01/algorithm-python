bb7b0a10779b36226c4b7cfb6231a7bd2c20aa91
esakik <social0430@gmail.com>
2020-04-28 22:57:23 +0900

    Refactoring

--- src/algorithms/sorting/bubble_sort.py
+++ src/algorithms/sorting/bubble_sort.py
@@ -3,8 +3,8 @@ class BubbleSort:
     @staticmethod
     def sort(data: list):
         for k in range(len(data) - 1):
-            for i in range(len(data) - 1, k, -1):
-                if data[i - 1] > data[i]:
-                    tmp = data[i - 1]
-                    data[i - 1] = data[i]
-                    data[i] = tmp
+            for i in range(len(data) - 1 - k):
+                if data[i] > data[i + 1]:
+                    tmp = data[i]
+                    data[i] = data[i + 1]
+                    data[i + 1] = tmp
--- src/algorithms/sorting/insertion_sort.py
+++ src/algorithms/sorting/insertion_sort.py
@@ -4,8 +4,8 @@ class InsertionSort:
     def sort(data: list):
         for i in range(1, len(data)):
             tmp = data[i]
-            k = i
-            while k > 0 and data[k - 1] > tmp:
-                data[k] = data[k - 1]
+            k = i - 1
+            while k >= 0 and data[k] > tmp:
+                data[k + 1] = data[k]
                 k -= 1
-            data[k] = tmp
+            data[k + 1] = tmp
